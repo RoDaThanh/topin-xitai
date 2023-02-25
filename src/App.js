13709fee3e35a905d6fb8a4cce33282d1e3d9ae9
RoDaThanh <117510172+RoDaThanh@users.noreply.github.com>
2023-02-25 17:34:23 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import Home from "./pages/Home"
+
+import ProductPage from './pages/ProductPage';
+import Home from './pages/Home';
+import {    createBrowserRouter,    RouterProvider,  } from "react-router-dom";
+import ProductList from './pages/ProductList';
+
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <Home/>,
+    },
+    {
+        path: "/products",
+        element: <ProductList/>,
+        
+      },
+      {
+        path:"/product/:id",
+        element: <ProductPage/>,
+      },
+  ]);
 
 const App = () => {
-    return <Home/>
+    return <RouterProvider router={router} />
 }
 
 export default App
